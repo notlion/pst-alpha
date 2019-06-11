31f0351a4d17293faa8c0eb01f33b3b161b9ddcd
Ryan Alexander <ryan@onecm.com>
2019-06-11 00:39:46 -0700

    Add WASD camera movement with pointer lock

--- web/renderer.js
+++ web/renderer.js
@@ -1,10 +1,37 @@
-import {mat4} from "./lib/gl-matrix-d6156a5/index.js";
+import {mat4,
+        quat,
+        vec2,
+        vec3} from "./lib/gl-matrix-d6156a5/index.js";
 import ParticleRenderer from "./particle-renderer.js";
 
 const DEG_TO_RAD = Math.PI / 180;
 
 class Camera {
   constructor() {
+    this.position = vec3.create();
+    this.positionPrev = vec3.create();
+    this.orientation = quat.create();
+
+    this.viewportAspectRatio = 1;
+    this.fieldOfViewY = 60;
+    this.clipNear = 0.01;
+    this.clipFar = 1000;
+
+    this.viewMatrix = mat4.create();
+    this.projectionMatrix = mat4.create();
+  }
+
+  translateWithLocalOrientation(direction, distance) {
+    const dir = vec3.transformQuat(vec3.create(), direction, quat.invert(quat.create(), this.orientation));
+    vec3.normalize(dir, dir);
+    vec3.scale(dir, dir, distance);
+    vec3.add(this.position, this.position, dir);
+  }
+
+  updateMatrices() {
+    mat4.fromTranslation(this.viewMatrix, this.position);
+    mat4.mul(this.viewMatrix, mat4.fromQuat(mat4.create(), this.orientation), this.viewMatrix);
+    mat4.perspective(this.projectionMatrix, this.fieldOfViewY * DEG_TO_RAD, this.viewportAspectRatio, this.clipNear, this.clipFar);
   }
 }
 
@@ -15,6 +42,12 @@ export class ParticleRendererElement extends HTMLElement {
     this.canvasElem = document.createElement("canvas");
     this.canvasElem.id = "particle-renderer-canvas";
     this.appendChild(this.canvasElem);
+
+    this.canvasElem.tabIndex = 0;
+    this.canvasElem.addEventListener("mousedown", event => this._onCanvasMouseDown(event));
+    this.canvasElem.addEventListener("keydown", event => this._onCanvasKeyDown(event));
+    this.canvasElem.addEventListener("keyup", event => this._onCanvasKeyUp(event));
+    this._canvasMouseMoveCallback = (event) => this._onCanvasMouseMove(event);
 
     this.isReady = false;
 
@@ -35,7 +68,8 @@ export class ParticleRendererElement extends HTMLElement {
       window.requestAnimationFrame(onFrame);
     };
 
-    this.canvasAspectRatio = 1;
+    this.camera = new Camera();
+    this.cameraMovementDirection = vec3.create();
 
     this._prevFrameTimeMillis = 0;
     this.timeMillis = 0;
@@ -46,8 +80,10 @@ export class ParticleRendererElement extends HTMLElement {
     const deltaTime = this._prevFrameTimeMillis === 0 ? 0 : timestamp - this._prevFrameTimeMillis;
     this._prevFrameTimeMillis = timestamp;
 
-    this.setViewMatrix(mat4.lookAt(mat4.create(), [ 0, 0, 3 ], [ 0, 0, 0 ], [ 0, 1, 0 ]));
-    this.setProjectionMatrix(mat4.perspective(mat4.create(), 60.0 * DEG_TO_RAD, this.canvasAspectRatio, 0.01, 1000));
+    this.camera.translateWithLocalOrientation(this.cameraMovementDirection, 0.025);
+    this.camera.updateMatrices();
+    this.setViewMatrix(this.camera.viewMatrix);
+    this.setProjectionMatrix(this.camera.projectionMatrix);
 
     if (!this.timeIsPaused) {
       this.timeMillis += deltaTime;
@@ -55,6 +91,69 @@ export class ParticleRendererElement extends HTMLElement {
     }
 
     this.module._render();
+  }
+
+  _onCanvasMouseDown(event) {
+    this.canvasElem.requestPointerLock();
+    this.canvasElem.addEventListener("mousemove", this._canvasMouseMoveCallback);
+    document.addEventListener("mouseup", (event) => {
+      this.canvasElem.removeEventListener("mousemove", this._canvasMouseMoveCallback);
+      document.exitPointerLock();
+    }, { once : true });
+  }
+
+  _onCanvasMouseMove(event) {
+    const rotation = quat.fromEuler(quat.create(), event.movementY * 0.25, event.movementX * 0.25, 0);
+    quat.mul(this.camera.orientation, rotation, this.camera.orientation);
+    quat.normalize(this.camera.orientation, this.camera.orientation);
+  }
+
+  _onCanvasKeyDown(event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "w":
+        this.cameraMovementDirection[2] = 1;
+        break;
+      case "s":
+        this.cameraMovementDirection[2] = -1;
+        break;
+      case "a":
+        this.cameraMovementDirection[0] = 1;
+        break;
+      case "d":
+        this.cameraMovementDirection[0] = -1;
+        break;
+      case "q":
+        this.cameraMovementDirection[1] = 1;
+        break;
+      case "e":
+        this.cameraMovementDirection[1] = -1;
+        break;
+    }
+  }
+
+  _onCanvasKeyUp(event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "w":
+        if (this.cameraMovementDirection[2] > 0) this.cameraMovementDirection[2] = 0;
+        break;
+      case "s":
+        if (this.cameraMovementDirection[2] < 0) this.cameraMovementDirection[2] = 0;
+        break;
+      case "a":
+        if (this.cameraMovementDirection[0] > 0) this.cameraMovementDirection[0] = 0;
+        break;
+      case "d":
+        if (this.cameraMovementDirection[0] < 0) this.cameraMovementDirection[0] = 0;
+        break;
+      case "q":
+        if (this.cameraMovementDirection[1] > 0) this.cameraMovementDirection[1] = 0;
+        break;
+      case "e":
+        if (this.cameraMovementDirection[1] < 0) this.cameraMovementDirection[1] = 0;
+        break;
+    }
   }
 
   rewind() {
@@ -65,7 +164,7 @@ export class ParticleRendererElement extends HTMLElement {
   updateLayout() {
     this.canvasElem.width = this.clientWidth;
     this.canvasElem.height = this.clientHeight;
-    this.canvasAspectRatio = this.canvasElem.width / this.canvasElem.height;
+    this.camera.viewportAspectRatio = this.canvasElem.width / this.canvasElem.height;
   }
 
   getSimulationShaderSource() {
