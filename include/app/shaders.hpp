b02e8e4db57b53dfe0107150d91fc71ca5438bde
Ryan Alexander <ryan@onecm.com>
2019-07-25 14:31:32 -0700

    Update inline shaders

--- include/app/shaders.hpp
+++ include/app/shaders.hpp
@@ -95,6 +95,7 @@ void main() {
 
 const char *shader_source_user_default_fragment = R"GLSL(void mainFragment(out vec4 oColor, in vec4 color, in vec2 texcoord) {
   oColor = color;
+  oColor.rg *= 0.85 + 0.15 * texcoord;
 }
 )GLSL";
 
