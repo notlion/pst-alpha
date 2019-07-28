004e3f7a2d5c962af37d2336e39421a512f55a84
Ryan Alexander <ryan@onecm.com>
2019-07-28 01:11:27 -0700

    Fix compilation on Windows/WSL. Switch to upstream Emscripten

--- emscripten/build_wasm.sh
+++ emscripten/build_wasm.sh
@@ -7,7 +7,7 @@ BUILD_DIR="build"
 mkdir -p ${BUILD_DIR}
 pushd ${BUILD_DIR}
 
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE="${EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake" $@
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE="${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" $@
 make -j4
 
 popd
